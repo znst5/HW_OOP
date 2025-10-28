fde34ba851cd4bfad454d261395e20b544749c58
znst5 <161199179+znst5@users.noreply.github.com>
2025-10-28 12:22:45 +0300

    done

--- HW_iter_gen.py
+++ HW_iter_gen.py
@@ -6,21 +6,26 @@ class FlatIterator:
 
     def __init__(self, list_of_list):
         self.list_of_list = list_of_list
+        self.outer_idx = 0
+        self.inner_idx = 0
 
     def __iter__(self):
-        self.cursor = -1
         return self
 
     def __next__(self):
-        self.flat_iterator_item = []
-        for items in self.list_of_list:
-            for item in range(len(items)):
-                self.flat_iterator_item.append(items[item])
-                self.cursor += 1
-        for i in range(len(self.flat_iterator_item)):
-            if i == self.cursor:
-                raise StopIteration
-        return self.flat_iterator_item[self.cursor]
+        while self.outer_idx < len(self.list_of_list):
+            current_list = self.list_of_list[self.outer_idx]
+
+            if self.inner_idx < len(current_list):
+                item = current_list[self.inner_idx]
+                self.inner_idx += 1
+                return item
+
+            else:
+                self.outer_idx += 1
+                self.inner_idx = 0
+
+        raise StopIteration
 
 
 def test_1():
@@ -47,35 +52,37 @@ if __name__ == '__main__':
 # Доработать функцию flat_generator. Должен получиться генератор, который принимает список списков и возвращает их плоское
 # представление. Функция test в коде ниже также должна отработать без ошибок.
 
-# import types
-#
-#
-# def flat_generator(list_of_lists):
-#
-#     ...
-#     yield
-#     ...
-#
-#
-# def test_2():
-#
-#     list_of_lists_1 = [
-#         ['a', 'b', 'c'],
-#         ['d', 'e', 'f', 'h', False],
-#         [1, 2, None]
-#     ]
-#
-#     for flat_iterator_item, check_item in zip(
-#             flat_generator(list_of_lists_1),
-#             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-#     ):
-#
-#         assert flat_iterator_item == check_item
-#
-#     assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-#
-#     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
-#
-#
-# if __name__ == '__main__':
-#     test_2()
+import types
+
+
+def flat_generator(list_of_lists):
+    current_list = []
+    for i in list_of_lists:
+        for j in i:
+            yield j
+            current_list.append(j)
+    return current_list
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
